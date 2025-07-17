d926263832dc57bf8fdc0e856c8246869deb13b6
WilliamFontaine <william.fontaine0308@gmail.com>
2025-07-17 18:24:25 +0200

    chore: remove test file

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,19 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'chore',
-        'perf',
-        'ci',
-        'build',
-        'revert'
-      ]
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf', 'ci', 'build', 'revert']
     ],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
